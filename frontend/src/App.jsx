6457e95c66be81a38261a48f6923b1c2dfc1106f
Arun Bohra <arunbohra12@gmail.com>
2022-05-22 03:29:40 +0530

    Add footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import { Route, Switch } from 'react-router-dom';
 import ProductList from './pages/product-list/product-list';
 import PageNotFound from './pages/404-page/404-page';
 import ProductAdd from './pages/product-add/product-add';
+import Footer from './components/footer/footer.component';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path='/' component={ProductList} />
-        <Route exact path='/add-product' component={ProductAdd} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <>
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route exact path='/add-product' component={ProductAdd} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </>
     );
   }
 }
---
+++ frontend/src/components/footer/footer.component.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className='footer'>Scandiweb Test assignment</footer>;
+};
+
+export default Footer;
